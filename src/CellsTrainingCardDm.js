aee7e890f62bdbd0aa6a4ca5820f5e89e9a00083
Daniel Cardona <cardonacalderondaniel@gmail.com>
2023-02-21 14:59:19 -0500

    agregando detalle de tarjetas

--- src/CellsTrainingCardDm.js
+++ src/CellsTrainingCardDm.js
@@ -1,7 +1,7 @@
 import { LitElement, } from 'lit-element';
 
 
-import { BGADPCardsGetV0 } from '@cells-components/bgadp-cards-v0/bgadp-cards-v0.js';
+import { BGADPCardsGetV0, BGADPCardsCardGetV0 } from '@cells-components/bgadp-cards-v0/bgadp-cards-v0.js';
 
 export class CellsTrainingCardDm extends LitElement {
   static get is() {
@@ -42,6 +42,27 @@ export class CellsTrainingCardDm extends LitElement {
         (error) => {
           this._fireEvents('request-cards-error', error);
         });
+  }
+
+
+  gerCardDetail(id) {
+
+    const config = {
+      host: this.host,
+      params: { 'card-id': id },
+      version: this.version
+    };
+    const _dataProvider = new BGADPCardsCardGetV0(config);
+
+    //Genera los elemento
+    _dataProvider.generateRequest()
+      .then((response) => {
+        this._fireEvents('card-detail-succes', response);
+      },
+        (error) => {
+          this._fireEvents('card-detail-error', error);
+        }
+      );
   }
 
   _fireEvents(eventName, details) {
